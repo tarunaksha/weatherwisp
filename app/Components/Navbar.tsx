a1aee75c49187c93ce9acf5dc512851da2a444df
tarunaksha <115142622+tarunaksha@users.noreply.github.com>
2024-07-15 14:57:02 +0530

    initial commit

---
+++ app/Components/Navbar.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { github } from "../utils/Icons";
+import ThemeDropdown from "./ThemeDropdown/ThemeDropdown";
+import SearchDialog from "./SearchDialog/SearchDialog";
+
+function Navbar() {
+  const router = useRouter();
+
+  return (
+    <div className="w-full py-4 flex items-center justify-between">
+      <div className="left-part"></div>
+      <div className="search-container flex shrink-0 w-full gap-2 sm:w-fit">
+        <SearchDialog />
+        <div className="btn-group flex items-center gap-2">
+          <ThemeDropdown />
+          <Button
+            className="source-code flex gap-2 items-center"
+            onClick={() =>
+              router.push("https://github.com/tarunaksha/weatherwisp")
+            }
+          >
+            {github} Source Code
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "./Components/Navbar";
+
+
+export default function Home() {
+  return (
+   <main className="mx-[1rem] lg:mx-[2rem] xl:mx-[6rem] 2xl:mx-[16rem] m-auto">
+    <Navbar />
+   </main>
+  );
+}
